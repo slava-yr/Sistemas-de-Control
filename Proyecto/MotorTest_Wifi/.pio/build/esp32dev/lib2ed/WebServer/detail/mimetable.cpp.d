c4426a5e08a1503abfa7fcd63ac6ece79b14c956
Salvador Yabar <salvador.yabar@ucalgary.ca>
2025-12-03 17:36:54 -0500

    update

---
+++ Proyecto/MotorTest_Wifi/.pio/build/esp32dev/lib2ed/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib2ed/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/Salvador/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/Salvador/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/Salvador/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
