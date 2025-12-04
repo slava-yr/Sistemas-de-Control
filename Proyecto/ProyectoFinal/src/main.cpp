8c97aca84622fd9e08d00cce93993c14389f1814
Salvador Yabar <salvador.yabar@ucalgary.ca>
2025-12-03 20:01:30 -0500

    updated

--- Proyecto/ProyectoFinal/src/main.cpp
+++ Proyecto/ProyectoFinal/src/main.cpp
@@ -11,8 +11,8 @@
 #include <WebServer.h>
 
 /* ====== CONFIGURAR WIFI ====== */
-const char* ssid     = "JOSE FUENTES_2.4G";
-const char* password = "#87654321#";
+const char* ssid     = "Salvador";
+const char* password = "candy123";
 
 /* ====== SERVIDOR WEB ====== */
 WebServer server(80);
@@ -36,11 +36,7 @@ WebServer server(80);
 #define EncAM2 26
 #define EncBM2 25
 
-// Filtro pasabajos
-#define wc 50
-#define Tf 0.005
-#define Resolucion 1801
-#define VM 11.1
+#define VM 11
 
 typedef struct {
   double periodo;
@@ -65,9 +61,10 @@ MotorVars motor1 = {1000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1080, 0.014, 1, 0};
 MotorVars motor2 = {1000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1083, 0.0142, 2, 0};
 
 // Variables globales de posición
-double phi;
+double phid, phi;
 double x, xd, xdp;
 double y, yd, ydp;
+bool fin_trayectoria;
 
 /* ====== PÁGINA HTML ====== */
 const char MAIN_page[] PROGMEM = R"=====( 
@@ -150,7 +147,7 @@ const char MAIN_page[] PROGMEM = R"=====(
     </table>
   </div>
 
-  <div class="nombre">José Emmanuel Fuentes Manrique</div>
+  <div class="nombre">Grupo Alfa buena maravilla, super onda dinamita, escuadrón lobo.</div>
 
   <script>
     async function actualizar() {
@@ -213,8 +210,8 @@ void isr_func(MotorVars *m, int enc, int resolution) {
   m->vel_1 = m->vel_ang;
 }
 
-void IRAM_ATTR ISR_FUN_M1() { isr_func(&motor1, EncAM1, Resolucion); }
-void IRAM_ATTR ISR_FUN_M2() { isr_func(&motor2, EncAM2, Resolucion); }
+void IRAM_ATTR ISR_FUN_M1() { isr_func(&motor1, EncAM1, motor1.resolucion); }
+void IRAM_ATTR ISR_FUN_M2() { isr_func(&motor2, EncAM2, motor2.resolucion); }
 
 void env_volt(double vp, uint8_t motor_id)
 {
@@ -267,8 +264,8 @@ void speed_control(void *pvParameters)
 
   double t = 0;
   double T = 0.001; // Periodo de muestreo
-  // double vel_d = 200; // 200 deg/s
 
+  m->vel_d = 200; 
   // Parámetros del controlador PI
   double Ti = 0.0625;
 
@@ -299,79 +296,28 @@ void speed_control(void *pvParameters)
   }
 }
 
-void seguimientoTrayectoria(void *pvParameters)
-{
-  // Sigue la trayectoria definida por generadorTrayectoria
-  // xd, xdp, yd, ydp, phi actualizados en generadorTrayectoria
-
-
-  double a = 12.3/100; // En m
-  double Tes = 3;
-  double K1 = 4/Tes, K2 = 4/Tes;
-  double x = 0, y = 0, av1 = 0, av2 = 0;
-  double r = 34.5/1000;
-  double d = 147/1000;
-  // referencias
-  double U_ref, W_ref, wd_ref, wi_ref;
-  // Errores
-  double xe = 0; 
-  double ye = 0;
- 
-  double t = 0; // t en segundos
-  double dt = 100; // dt en ms
-  while(1)
-  {
-    generadorTrayectoria(t); // Actualizar valores
-
-    x = x + a*cos(phi);
-    y = y + a*sin(phi);
-
-    // Error
-    xe = xd - x; 
-    ye = yd - y;
-
-    // Control de trayectoria
-    av1 = K1*xe + xdp;
-    av2 = K2*ye + ydp;
-
-    U_ref = cos(phi)*av1 + sin(phi)*av2;
-    W_ref = -sin(phi)/a*av1 + cos(phi)/a*av2;
-
-    // Actualizar velocidad deseada para los motores
-    motor1.vel_d = (U_ref*2/r - W_ref*d/r)/2*180/PI;
-    motor2.vel_d = (U_ref*2/r + W_ref*d/r)/2*180/PI;
-
-    t += dt/1000; // Actualizar el tiempo
-    vTaskDelay(dt); // Probar con diferentes valores
-  }
-}
-
 void generadorTrayectoria(double t)
 {
-  // Actualiza xd, xdp, yd, ydp, phi
+  double V_ref = 0.1; 
+  double V_ref_g = 0.05;
+  double distancia_s1 = 0.22; 
+  double radio_s2 = 0.206; 
+  double radio_s4 = 0.206; 
+  double distancia_s5 = 0.40; 
+  double radio_s6 = 0.125; 
+  double radio_s7 = 0.175; 
+  double distancia_s8 = 0.20; 
 
-  // Definiciones
-  double V_ref = 0.1; // velocidad deseada (m/s)
-  double distancia_s1 = 0.22; // Recta inicial
-  double radio_s2 = 0.10; // 1ra U (Izquierda)
-  double distancia_s3 = 0.05; // Recta retorno corta
-  double radio_s4 = 0.10; // 2da U (Panza/Derecha)
-  double distancia_s5 = 0.40; // Recta larga
-  double radio_s6 = 0.10; // 3ra U (Final/Derecha)
-  double radio_s7 = 0.10; // Giro 90 grados (Izquierda)
-  double distancia_s8 = 0.10; // Recta final bajando
-
-  // Tiempos (en segundos)
-  double dt1, dt2, dt3, dt4, dt5, dt6, dt7, dt8; 
-  double T1, T2, T3, T4, T5, T6, T7, T8;
+  double dt1, dt2, dt3, dt4, dt5, dt6, dt7; 
+  double T1, T2, T3, T4, T5, T6, T7;
 
   dt1 = distancia_s1 / V_ref;
-  dt2 = (PI * radio_s2) / V_ref; 
-  dt4 = (PI * radio_s4) / V_ref; 
-  dt5 = distancia_s5 / V_ref;
-  dt6 = (PI * radio_s6) / V_ref; 
-  dt7 = (0.5 * PI * radio_s7) / V_ref;
-  dt8 = distancia_s8 / V_ref;
+  dt2 = (PI * radio_s2) / V_ref_g;
+  dt3 = (PI * radio_s4) / V_ref_g; 
+  dt4 = distancia_s5 / V_ref;
+  dt5 = (0.5 * PI * radio_s6) / V_ref_g; 
+  dt6 = (0.5 * PI * radio_s7) / V_ref_g;
+  dt7 = distancia_s8 / V_ref;
 
   T1 = dt1;
   T2 = T1 + dt2;
@@ -380,41 +326,177 @@ void generadorTrayectoria(double t)
   T5 = T4 + dt5;
   T6 = T5 + dt6;
   T7 = T6 + dt7;
-  T8 = T7 + dt8;
 
-  // Posiciones finales
+  double xf1 = distancia_s1; 
+  double yf2 = radio_s2*2; 
+  double yf3 = radio_s4*2 + yf2; 
+  double xf4 = xf1 + distancia_s5; 
+  double xf5 = xf4 + radio_s6;
+  double yf5 = yf3 - radio_s6;
+  
+  // Calculated endpoints for the new segments
+  double xf6 = xf5 + radio_s7; 
+  double yf6 = yf5 - radio_s7; 
+  double xf7 = xf6 + distancia_s8;
 
-  /*====== CONTROL ========*/
+  double V=V_ref; 
+  double t_local;
+  double phi_p = 0;
+  double theta_start;
 
+  if (t <= T1)
+  {
+    xd = V_ref * t;
+    yd = 0;
+    xdp = V_ref; 
+    ydp = 0; 
+    phid = 0;
+  }
+  else if (t > T1 && t <= T2)
+  {
+    t_local = t - T1;
+    phi_p = V_ref_g / radio_s2; 
+    phid = phi_p * t_local; 
+    xd = xf1 + radio_s2*sin(phid);
+    yd = radio_s2 - radio_s2*cos(phid);
+    xdp = V_ref_g*cos(phid); 
+    ydp = V_ref_g*sin(phid);
+  }
+  else if (t > T2 && t <= T3)
+  {
+    t_local = t - T2;
+    phi_p = V_ref_g / radio_s4;
+    theta_start = 3*PI/2;
+    phid = theta_start - phi_p * t_local; 
+    xd = xf1 + radio_s4*cos(phid);
+    yd = yf2 + radio_s4 + radio_s4*sin(phid);
+    xdp = -radio_s4 * sin(phid) * (-phi_p);
+    ydp = radio_s4 * cos(phid) * (-phi_p);
+  }
+  else if (t > T3 && t <= T4)
+  {
+    phid = 0; 
+    t_local = t - T3;
+    xd = xf1 + V_ref*t_local;
+    yd = yf3;
+    xdp = V_ref; 
+    ydp = 0; 
+  }
+  else if (t > T4 && t <= T5)
+  {
+    t_local = t - T4;
+    phi_p = -V_ref_g / radio_s6; 
+    phid = 0 + phi_p*t_local; 
+    xd = xf4 - radio_s6*sin(phid); 
+    yd = (yf3 - radio_s6) + radio_s6*cos(phid);
+    xdp = V_ref_g*cos(phid); 
+    ydp = V_ref_g*sin(phid);
+  }
+  else if (t > T5 && t <= T6)
+  {
+    // Segment 6: Left Turn (CCW) from South to East
+    t_local = t - T5;
+    phi_p = V_ref_g/radio_s7; 
+    
+    // Angle on the circle (Starts at PI, goes to 3PI/2)
+    double theta = PI + phi_p*t_local; 
+    
+    // Center is at (xf5 + r, yf5)
+    xd = (xf5 + radio_s7) + radio_s7*cos(theta); 
+    yd = yf5 + radio_s7*sin(theta);
+    
+    phid = -PI/2 + phi_p*t_local; // Robot heading (-90 to 0)
+    xdp = V_ref_g*cos(phid); 
+    ydp = V_ref_g*sin(phid);
+  }  
+  else if (t > T6 && t <= T7)
+  {
+    // Segment 7: Straight line East
+    t_local = t - T6;
+    phid = 0;
+    xd = xf6 + V*t_local;
+    yd = yf6;
+    xdp = V; 
+    ydp = 0; 
+  }
 
-  // Completar
+  else
+  {
+    xd = xf7;
+    yd = yf6;
+    xdp = 0; 
+    ydp = 0;
+    phid = 0;  
+
+    fin_trayectoria = true;
+  }
 }
 
+void seguimientoTrayectoria(void *pvParameters)
+{
+  // Sigue la trayectoria definida por generadorTrayectoria
+  // xd, xdp, yd, ydp, phi actualizados en generadorTrayectoria
+  double a = 12.3/100; // En m
+  double Tes = 1.5;
+  double K1 = 4/Tes, K2 = 4/Tes;
+  double x = 0, y = 0, av1 = 0, av2 = 0;
 
-// void printSpeed(void *pvParameters)
-// {
-//   const uint16_t delay_ms = 5; // 1 ms
-//   uint16_t t = 0;
-//   char msg_temp[64];
-//   while (1)
-//   {
-//     Serial.print(t);
-//     Serial.print("  ");
-//     Serial.print("Motor_L");
-//     Serial.print("  ");
-//     Serial.print(motor1.vmotor);
-//     Serial.print("  ");
-//     Serial.print(motor1.vel_ang_median);
-//     Serial.print("  ");
-//     Serial.print("Motor_R");
-//     Serial.print("  ");
-//     Serial.print(motor2.vmotor);
-//     Serial.print("  ");
-//     Serial.println(motor2.vel_ang_median);
-//     t += delay_ms;
-//     vTaskDelay(delay_ms); 
-//   }
-// }
+  double r = 6.75/200; // m
+  double d = 24.6/100; // m 
+  // referencias
+  double U_ref, W_ref, wd_ref, wi_ref;
+  // Errores
+  double xe = 0; 
+  double ye = 0;
+ 
+  double t = 0; // t en segundos
+  double dt = 100; // dt en ms
+
+  double vrobot, wrobot, phi_est;
+  while(1)
+  {
+    generadorTrayectoria(t); // Actualizar valores
+    
+    if (fin_trayectoria)
+    {
+      motor1.vel_d = 0;
+      motor2.vel_d = 0;
+    }
+
+    else
+    {
+      // Estimaciones de posición y trayectoria
+      vrobot = (r/2.0) * (motor2.vel_ang_median + motor1.vel_ang_median)*PI/180;
+      wrobot = ((r/d) * (motor2.vel_ang_median - motor1.vel_ang_median))*PI/180;
+      phi_est += wrobot * dt/1000;
+
+      if (phi_est > PI) phi_est -= 2*PI;
+      if (phi_est < -PI) phi_est += 2*PI;
+      
+      x += vrobot * cos(phi_est) * dt/1000;
+      y += vrobot * sin(phi_est) * dt/1000;
+      phi = phi_est;
+
+      // Error
+      xe = xd - x; 
+      ye = yd - y;
+
+      // Control de trayectoria
+      av1 = K1*xe + xdp;
+      av2 = K2*ye + ydp;
+
+      U_ref = cos(phi)*av1 + sin(phi)*av2;
+      W_ref = -sin(phi)/a*av1 + cos(phi)/a*av2;
+
+      // Actualizar velocidad deseada para los motores
+      motor1.vel_d = (U_ref*2/r - W_ref*d/r)/2*180/PI;
+      motor2.vel_d = (U_ref*2/r + W_ref*d/r)/2*180/PI;
+    }
+    
+    t += dt/1000; // Actualizar el tiempo
+    vTaskDelay(dt); 
+  }
+}
 
 /* ====== HANDLERS WEB ====== */
 
@@ -429,10 +511,11 @@ void handleStatus() {
 
   String json = "{";
   json += "\"tension_izq\":" + String(motor1.vmotor,       3) + ",";
-  json += "\"tension_der\":" + String(motor2.vmotor,       3) + ",";
+  json += "\"tension_der\":" + String(phi,       3) + ",";
   json += "\"tiempo\":"      + String(t_s,                  3) + ",";
   json += "\"vel_izq\":"     + String(motor1.vel_ang_median, 3) + ",";
   json += "\"vel_der\":"     + String(motor2.vel_ang_median, 3);
+  
   json += "}";
 
   server.send(200, "application/json", json);
@@ -480,7 +563,7 @@ void setup() {
   // Tasks
   xTaskCreatePinnedToCore(speed_control, "Speed_Control_M1", 4000, &motor1, 2, NULL, APP_CPU_NUM); // Speed control motor 1
   xTaskCreatePinnedToCore(speed_control, "Speed_Control_M2", 4000, &motor2, 2, NULL, APP_CPU_NUM); // Speed control motor 1
-  xTaskCreatePinnedToCore(seguimientoTrayectoria, "SeguimientoTrayectoria", 4000, NULL, 1, NULL, APP_CPU_NUM); // Speed control motor 1
+  xTaskCreatePinnedToCore(seguimientoTrayectoria, "SeguimientoTrayectoria", 4000, NULL, 1, NULL, APP_CPU_NUM); // Seguimiento de la trayectoria
   // xTaskCreatePinnedToCore(printSpeed, "printSpeed", 4000, NULL, 1, NULL, APP_CPU_NUM); // Print speeds
 
   Serial.println("Setup done!");
